d8257dd49b0d2f291d8b20368171a5dc9dcf5430
Cat-Kanangnut <130201193+Kanangnut@users.noreply.github.com>
2023-09-22 01:20:08 +0800

    Update 30_Python_Codesignal-Create Spiral Matrix.py

--- 30_Python_Codesignal-Create Spiral Matrix.py
+++ 30_Python_Codesignal-Create Spiral Matrix.py
@@ -1,3 +1,34 @@
+Create Spiral Matrix:
+
+A spiral matrix is a square matrix of size n × n. It contains all the integers in range from 1 to n * n so that number 1 is written in the bottom right corner, and all other numbers are written in increasing order spirally in the counterclockwise direction.
+
+Given the size of the matrix n, your task is to create a spiral matrix.
+
+Example
+
+For n = 3, the output should be
+
+solution(n) = [[5, 4, 3],
+               [6, 9, 2],
+               [7, 8, 1]]
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+Matrix size, a positive integer.
+
+Guaranteed constraints:
+3 ≤ n ≤ 75.
+
+[output] array.array.integer
+
+A spiral matrix of size n.
+
+Solution:
 def solution(n):
     dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
     curDir = 0
