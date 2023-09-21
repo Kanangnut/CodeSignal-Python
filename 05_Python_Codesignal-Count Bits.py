50fd930d9da5b139493f194e754151f8c78d328a
Cat-Kanangnut <130201193+Kanangnut@users.noreply.github.com>
2023-09-21 23:23:13 +0800

    Update 05_Python_Codesignal-Count Bits.py

--- 05_Python_Codesignal-Count Bits.py
+++ 05_Python_Codesignal-Count Bits.py
@@ -1,2 +1,32 @@
+Count Bits:
+Implement a function that, given an integer n, uses a specific method on it and returns the number of bits in its binary representation.
+
+Note: in this task and most of the following tasks you will be given a code snippet with some part of it replaced by the ellipsis (...). Only this part is allowed to be changed.
+
+Example
+
+For n = 50, the output should be
+solution(n) = 6.
+
+5010 = 1100102, a number that consists of 6 digits. Thus, the output should be 6.
+
+Input/Output
+
+[execution time limit] 4 seconds (py)
+
+[memory limit] 1 GB
+
+[input] integer n
+
+A positive integer.
+
+Guaranteed constraints:
+1 ≤ n ≤ 109.
+
+[output] integer
+
+The number of bits in binary representation of n.
+
+Solution:
 def solution(n):
     return n.bit_length()
